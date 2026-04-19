9f88d2e140cb734dc480c71411d117a9dc777406
psyclyx <me@psyclyx.xyz>
2026-04-18 22:05:41 -0700

    build: add harfbuzz to nix shell

--- shell.nix
+++ shell.nix
@@ -1,27 +1,29 @@
 let
   sources = import ./npins;
   flake-compat = import sources.flake-compat;
-  zig-flake = (flake-compat { src = sources.zig-overlay; }).defaultNix;
+  zig-flake = (flake-compat {src = sources.zig-overlay;}).defaultNix;
   pkgs = import sources.nixpkgs-unstable {
-    overlays = [ zig-flake.overlays.default ];
+    overlays = [zig-flake.overlays.default];
   };
 in
-pkgs.mkShell {
-  packages = with pkgs; [
-    zigpkgs.master
-    pkg-config
-    libGL
-    libglvnd
-    wayland
-    wayland-protocols
-    wayland-scanner
-    libxkbcommon
-  ];
+  pkgs.mkShell {
+    packages = with pkgs; [
+      zigpkgs.master
+      pkg-config
+      libGL
+      libglvnd
+      wayland
+      wayland-protocols
+      wayland-scanner
+      libxkbcommon
+      harfbuzz
+    ];
 
-  LD_LIBRARY_PATH = with pkgs; pkgs.lib.makeLibraryPath [
-    libGL
-    libglvnd
-    wayland
-    libxkbcommon
-  ];
-}
+    LD_LIBRARY_PATH = with pkgs;
+      pkgs.lib.makeLibraryPath [
+        libGL
+        libglvnd
+        wayland
+        libxkbcommon
+      ];
+  }
